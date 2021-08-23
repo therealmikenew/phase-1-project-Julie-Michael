4103dd9d23a9b54601c0171d222606cc36cdb75e
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-23 17:38:22 -0400

    updated HTML

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const BASE_URL = 'https://elephant-api.herokuapp.com/elephants';
 
-function postCheck() {
-    fetch('https://elephant-api.herokuapp.com/elephants/random').then((response) => {response.json()}).then(data => console.log(data))
-}
+// function postCheck() {
+//     fetch('https://elephant-api.herokuapp.com/elephants/random', {
+//         method: "GET",
+//         mode: "no-cors",
+//         header: {
+    
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
