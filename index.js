4c808ec1d271e127c93f5e9999242b8d2fc79775
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-23 17:03:42 -0400

    Updated HTML

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const BASE_URL = 'https://elephant-api.herokuapp.com/elephants';
 
 function postCheck() {
-    fetch(BASE_URL, {
-        method: "GET",
-        mode: 'no-cors',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        }
-        // body: JSON.stringify
-        })
-    .then((response) => {response.json()})
-    .then(data => console.log(data))
+    fetch('https://elephant-api.herokuapp.com/elephants/random').then((response) => {response.json()}).then(data => console.log(data))
 }
