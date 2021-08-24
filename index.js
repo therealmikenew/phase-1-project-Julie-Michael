0e67dba37fc03e8a2b166b84714820b7486dd37f
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-24 09:54:22 -0400

    Updated index.html, index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const BASE_URL = 'https://elephant-api.herokuapp.com/elephants';
+const BASE_URL = 'https://official-joke-api.appspot.com/random_ten';
 
-// function postCheck() {
-//     fetch('https://elephant-api.herokuapp.com/elephants/random', {
-//         method: "GET",
-//         mode: "no-cors",
-//         header: {
-    
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-// }
+function getJokes() {
+    fetch(BASE_URL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+
+
+//addListner function that upon DOMContentLoaded will fetch data; in this function, we will copy over data from API to json
+
+//function that will add joke to the DOM; grab setup div and punchline div; eventlistner function that when clicked, will unhide punchline div
+
+//eventlistener for the rating that when submitted, send PATCH to json
+
+//eventlistener for next button which will have cb function, push new joke to DOM
