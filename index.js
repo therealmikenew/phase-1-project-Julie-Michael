ef61e4ce6aad166cfdc69fe4346068e8d092c89d
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-24 17:02:23 -0400

    added in some functions(getJokes, postJokes, addJokes)

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const BASE_URL = 'https://official-joke-api.appspot.com/random_ten';
+const LOCAL_URL = 'http://localhost:3000/';
+let JOKEDATA = [];
 
 function getJokes() {
     fetch(BASE_URL)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => data.forEach(jokeObj => JOKEDATA.push(jokeObj)))
+}
+// working on adding postJokes .forEach(jokeObj => postJokes(jokeObj))
+function postJokes(jokeObj) {
+    console.log(jokeObj);
+    debugger;
+    fetch(LOCAL_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jokeObj)
+    })
+    .then(response => response.json())
+    .then(joke => console.log(joke))
 }
 
-
-
 //addListner function that upon DOMContentLoaded will fetch data; in this function, we will copy over data from API to json
+document.addEventListener("DOMContentLoaded", () => {
+    getJokes();
+    // postJokes(JOKEDATA);
+})
 
 //function that will add joke to the DOM; grab setup div and punchline div; eventlistner function that when clicked, will unhide punchline div
+function addJoke(){
+    const setup = document.getElementById('setup');
+    const punchline = document.getElementById('hidden-punchline');
+    debugger;
+    setup.innerText = JOKEDATA[0].setup;
+    punchline.innerText = JOKEDATA[0].punchline;
+    const punchBttn = document.getElementById('punchBttn');
+
+
+}
 
 //eventlistener for the rating that when submitted, send PATCH to json
 
