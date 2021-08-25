1255b14a65d6fa8c18caa22edbccd5f9079bc6f2
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-25 14:01:00 -0400

    Cleaned up code and slight organization

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
 const BASE_URL = 'https://official-joke-api.appspot.com/random_ten';
-const LOCAL_URL = 'http://localhost:3000/jokes';
+
 let JOKEDATA = [];
 
 document.addEventListener("DOMContentLoaded", () => {
     getJokes();
-    //addJoke();
-    //postJokes(JOKEDATA);
-    const ratings = document.getElementById("ratings")
+
+    const ratings = document.getElementById("ratings");
 
     ratings.addEventListener('submit', (e) => {
         e.preventDefault();
-        const inputRating = document.getElementById('input-rating').value
-        JOKEDATA[0].rating = inputRating
+        const inputRating = document.getElementById('input-rating').value;
+        JOKEDATA[0].rating = inputRating;
     
-        console.log(JOKEDATA[0].rating)
+        console.log(JOKEDATA[0].rating);
         ratings.reset();
 
-        const ratingAvg = document.getElementById('ratings-total')
-        ratingAvg.innerText = `Rating Average:${inputRating}`
+        const ratingAvg = document.getElementById('ratings-total');
+        ratingAvg.innerText = `Rating Average:${inputRating}`;
 
-        //debugger;
+        //add code to average a joke rating and show the average in the DOM
+
+    })
+
+//eventlistener for next button which will have cb function, push new joke to DOM.
+
+//button should cycle through the objects in our JOKEDATA global array. once it reaches the end, it starts on index 0 again.
 
 
 })
-})
 
+//function that will add joke to the DOM; grab setup div and punchline div; eventlistner function that when clicked, will unhide punchline div
+function addJoke(obj){
+    //debugger;
+    const jokeSetup = document.getElementById('setup');
+    const punchline = document.getElementById('hidden-punchline');
+    //debugger;
+    jokeSetup.innerText = obj.setup;
+    punchline.innerText = obj.punchline;
+    const punchBttn = document.getElementById('punchBttn');
+    
+    //eventlistener for the rating that when submitted, Waiting to do this later(send PATCH to json)
+    punchBttn.addEventListener('click', (e) => {
+        const hiddenElement = document.getElementById("hidden-punchline");
+        if(hiddenElement.hasAttribute ('hidden')) {
+        hiddenElement.removeAttribute('hidden');
+        } else {
+        hiddenElement.setAttribute('hidden', true);
+        }
+        // fyi <tag or id>.removeAttribute('hidden');
+        // to add <tag or id>.setAttribute('hidden', true)
+    })
+}
 
 function getJokes() {
     fetch(BASE_URL)
     .then(response => response.json())
     .then(data => {
         data.forEach(jokeObj => {  
-            JOKEDATA.push(jokeObj)
+            JOKEDATA.push(jokeObj);
         }); 
-    addJoke(JOKEDATA[0]) 
+    addJoke(JOKEDATA[0]);
     });
 }
+
+
+// ----------STRETCH GOALS SECION----------
+
+//  2) add a new joke to our JOKEDATA array
+
+//  3) See all jokes that are rated, which we can rate them again
+
+
+//  code below is not needed, but can be worked on later
+//  Julie would like it to work, but again, it's not needed
+// --------------------------------------------------------------------
+
+//addListner function that upon DOMContentLoaded will fetch data; in this function, we will copy over data from API to json
 // working on adding postJokes  .forEach(jokeObj => JOKEDATA.push(jokeObj)))
-// function postJokes(jokeObj) {
+// const LOCAL_URL = 'http://localhost:3000/jokes';
+//function postJokes(jokeObj) {
 //     console.log(jokeObj);
 //     fetch(LOCAL_URL, {
 //         method: "POST",
@@ -49,36 +90,3 @@ function getJokes() {
 //     .then(response => response.json())
 //     .then(joke => addJoke(jokeObj))
 // }
-
-//addListner function that upon DOMContentLoaded will fetch data; in this function, we will copy over data from API to json
-
-
-//function that will add joke to the DOM; grab setup div and punchline div; eventlistner function that when clicked, will unhide punchline div
-function addJoke(obj){
-    //debugger;
-    const jokeSetup = document.getElementById('setup');
-    const punchline = document.getElementById('hidden-punchline');
-    //debugger;
-    jokeSetup.innerText = obj.setup;
-    punchline.innerText = obj.punchline;
-    const punchBttn = document.getElementById('punchBttn');
-
-    punchBttn.addEventListener('click', (e) => {
-        const hiddenElement = document.getElementById("hidden-punchline");
-        if(hiddenElement.hasAttribute ('hidden')) {
-        hiddenElement.removeAttribute('hidden');
-        } else {
-        hiddenElement.setAttribute('hidden', true)
-        }
-    
-    })
-
-// fyi <tag or id>.removeAttribute('hidden');
-// to add <tag or id>.setAttribute('hidden', true)
-}
-
-
-//eventlistener for the rating that when submitted, send PATCH to json
-
-//eventlistener for next button which will have cb function, push new joke to DOM
-
