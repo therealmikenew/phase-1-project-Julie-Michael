e04192fadb70f78377911eb603a22ec2311760df
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-26 17:58:28 -0400

    add in getAvgRating function.

--- index.js
+++ index.js
@@ -4,45 +4,40 @@ const LOCAL_URL = 'http://localhost:3000/';
 let JOKEDATA = [];
 let i = 0;
 
+// clean up code, set all functions to be ouside of DOMContent Loaded, but have them invoke there.
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const punchline = document.getElementById("hidden-punchline");
 
     getJokes();
 
-    const ratings = document.getElementById("ratings");
+    const ratingSection = document.getElementById("ratings");
 
-    ratings.addEventListener('submit', (e) => {
+    ratingSection.addEventListener('submit', (e) => {
         e.preventDefault();
-        const inputRating = document.getElementById('input-rating').value;
-        JOKEDATA[i].rating = inputRating;
-    
-        console.log(JOKEDATA[i].rating);
-        ratings.reset();
-
-        const ratingAvg = document.getElementById('ratings-total');
-        ratingAvg.innerText = `Rating Average: ${inputRating}`;
-
-        postJokes(JOKEDATA[0])
-
+        getAvgRating(JOKEDATA[i].rating, ratingSection);
+        // debugger;
         //add code to average a joke rating and show the average in the DOM
     })
 
     //eventlistener for next button which will have cb function, push new joke to DOM.
 
-const nextBtn = document.getElementById('nextBtn');
+    const nextBtn = document.getElementById('nextBtn');
 
-nextBtn.addEventListener('click', function() {
-       
+    nextBtn.addEventListener('click', function() {
+       //button should cycle through the objects in our JOKEDATA global array. once it reaches the end, it starts on index 0 again.
         
+        console.log(i);
         if (i < JOKEDATA.length) {
             i++;
-            addJoke(JOKEDATA[i])
+            console.log(i);
+            addJoke(JOKEDATA[i]);
         } else {
             i = 0;
-            getJokes()
+            getJokes();
         }
-        //debugger;
+        debugger;//THERE IS A BUG, SEE IF YOU CAN FIND IT
 
     });
     const punchBttn = document.getElementById('punchBttn');
@@ -60,9 +55,9 @@ nextBtn.addEventListener('click', function() {
     })
 
 
-const submitJokeBttn = document.querySelector("#submit")
+    const submitJokeBttn = document.querySelector("#submit")
 
-submitJokeBttn.addEventListener('click', submitJoke)
+    submitJokeBttn.addEventListener('click', submitJoke)
 
 
 })
@@ -83,9 +78,32 @@ function addJoke(obj){
     
 }
 
+// sends back the average rating, if it was rated. if not, send our their rating
+// fix up HTML wording
+function getAvgRating(joke, ratingSection){
+    const inputRating = document.getElementById('input-rating').value;
+    const showRating = document.getElementById('ratings-total');
+        if (JOKEDATA[i].rating) {
+            // debugger;
+            const rateArr = [...JOKEDATA[i].rating, inputRating];
+            const rateTotal = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+            const rateAvg = (rateArr.reduce(rateTotal))/(rateArr.length);  
+            console.log(rateArr);
+            patchJokes(rateArr);
+            showRating.textContent = `Rating average: ${rateAvg}`;
+            
+            // debugger;
+        } else {
+            const jokeObj = {...JOKEDATA[i], rating: inputRating}
+            postJokes(jokeObj);
+            //debugger;
+            showRating.textContent = `Thank you for rating: ${inputRating}`;
+        }
+        // debugger;
+        ratingSection.reset();
+}
 
-//button should cycle through the objects in our JOKEDATA global array. once it reaches the end, it starts on index 0 again.
-
+// ---------CODE BELOW FOR GET, POST, PATCH, DELETE, ETC...--------------------------------------//
 
 function getJokes() {
     fetch(BASE_URL)
@@ -100,7 +118,7 @@ function getJokes() {
 }
 
 function postJokes(jokeObj) {
-    console.log(jokeObj);
+    // debugger;
     fetch(LOCAL_URL + 'jokes', {
         method: "POST",
         headers: {
@@ -110,15 +128,50 @@ function postJokes(jokeObj) {
     })
     .then(response => response.json())
     .then(joke => console.log(joke))
+    .catch(error => {
+        console.error('Joke already exists. sending to a PATCH. ERROR: ', error);
+        console.log("can you see me")
+        patchJokes(jokeObj);
+    })
+}
+// postJoke looks too much like postJokes. can we change it to postNewJokes
+function postJoke (newJoke) {
+    // debugger;
+
+    const configJoke = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newJoke)
+    };
+
+    fetch(LOCAL_URL + 'newJokes', configJoke).then(resp => resp.json())
+    .then(newJokeData => console.log(newJokeData))
 }
 
+// incase you want to play with this. using to bring jokes into main section to re-rate
+function patchJokes(rating){
+    console.log(rating);
+    //const rating = jokeObj.rating
+    debugger;
+    fetch(LOCAL_URL + `jokes/${JOKEDATA[i].id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({rating})
+    })
+    .catch(error => {console.error('PATCH ERROR: ', error)}
+    )
+}
 // ----------STRETCH GOALS SECION----------
 
 //  2) add a new joke to our JOKEDATA array
 
 function submitJoke (e) {
     e.preventDefault();
-    debugger;
+    // debugger;
 
     let setup = document.querySelector('#setup-input').value
     let punchline = document.querySelector('#punchline-input').value
@@ -135,32 +188,9 @@ function submitJoke (e) {
 }
 
 
-function postJoke (newJoke) {
-    debugger;
-
-    const configJoke = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newJoke)
-    };
-
-    fetch(LOCAL_URL + 'newJokes', configJoke).then(resp => resp.json())
-    .then(newJokeData => console.log(newJokeData))
-}
-
 
 
 
 
 //  3) See all jokes that are rated, which we can rate them again
-
-
-//  code below is not needed, but can be worked on later
-//  Julie would like it to work, but again, it's not needed
-// --------------------------------------------------------------------
-
-//addListner function that upon DOMContentLoaded will fetch data; in this function, we will copy over data from API to json
-// working on adding postJokes  .forEach(jokeObj => JOKEDATA.push(jokeObj)))
-
+// working on it
